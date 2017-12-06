544b73bf1fe52fcc1fc1c82518d2f337c234590e
MisterHackerr <33703036+MisterHackerr@users.noreply.github.com>
2017-12-06 15:27:42 +0100

    Update navire.cpp

--- navire.cpp
+++ navire.cpp
@@ -43,7 +43,99 @@ Color Navire::getCouleur() const { return couleurNavire; }
   
 void Navire::setEtat(bool c) { etat = c; }
 
-void Navire::deplacerNavire(int x, int y) { }
+void Navire::suprime_navire(Window& W, Color col){
+for(int i=0;i< this->nbCases;i++)
+    tabCases[i].suprime_case(W,col);
+}
+
+int Navire::Y_min_case_navire(){
+int minY=0;
+minY= tabCases[0].getY();
+for(int i=1;i< this->nbCases;i++)
+if(tabCases[i].getY()<= minY){
+minY=tabCases[i].getY();}
+
+return minY;
+}
+
+int Navire::Y_max_case_navire(){
+int maxY=0;
+maxY= tabCases[0].getY();
+for(int i=1;i< this->nbCases;i++)
+if(tabCases[i].getY()>= maxY){
+maxY=tabCases[i].getY();}
+
+return maxY;
+}
+
+
+int Navire::X_min_case_navire(){
+int minX=0;
+minX= tabCases[0].getX();
+for(int i=1;i< this->nbCases;i++)
+if(tabCases[i].getX()<= minX){
+minX=tabCases[i].getX();}
+
+return minX;
+}
+
+int Navire::X_max_case_navire(){
+int maxX=0;
+maxX= tabCases[0].getY();
+for(int i=1;i< this->nbCases;i++)
+if(tabCases[i].getX()>= maxX){
+maxX=tabCases[i].getX();}
+
+return maxX;
+}
+
+
+
+
+
+
+
+void Navire::deplacerNavireHaut(void) {
+    for(int i=0;i< this->nbCases;i++)
+    tabCases[i].setY( tabCases[i].getY()-1);
+}
+void Navire::deplacerNavireBas(void) {
+    for(int i=0;i< this->nbCases;i++)
+    tabCases[i].setY( tabCases[i].getY()+1);
+}
+void Navire::deplacerNavireDroite(void) {
+    for(int i=0;i< this->nbCases;i++)
+    tabCases[i].setX( tabCases[i].getX()+1);
+}
+
+void Navire::deplacerNavireGauche(void) {
+    for(int i=0;i< this->nbCases;i++)
+    tabCases[i].setX( tabCases[i].getX()-1);
+}
+
+  void Navire::PivoterNavireGauche(){
+  int sauv=0;
+  for(int i=0;i< this->nbCases;i++){
+    sauv=tabCases[i].getX();
+    tabCases[i].setX( tabCases[i].getY());
+    tabCases[i].setY( sauv);
+    }
+  }
+
+
+  void Navire::PivoterNavireDroite(){
+   int sauv=0;
+  for(int i=0;i< this->nbCases;i++){
+    sauv=tabCases[i].getX();
+    tabCases[i].setX( tabCases[i].getY());
+    tabCases[i].setY( sauv);
+    }
+  }
+
+
+
+
+
 
 // accesseurs en interrogation
 int Navire::nb_cases_touchees() const {int k = 0; for(int i = 0; i < nbCases; i++) { if (tabCases[i].getTouchee() == true) k ++; } return k; }
